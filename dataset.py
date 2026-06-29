370474eef7e122025e7efc753de27cc996a1c584
Yasmina <bouzianeya@cy-tech.fr>
2026-06-29 23:23:15 +0200

    add validation with dice metric

--- dataset.py
+++ dataset.py
@@ -5,7 +5,7 @@ import os
 import glob
 
 
-def get_dataloaders(data_dir: str):
+def get_bts_data_lists(data_dir: str):
     """
     Scans the BraTS 2020 directory and groups the 4 MRI modalities for each patient.
     Returns a list of dictionaries containing paths to the stacked images and their labels.
@@ -36,32 +36,33 @@ def get_dataloaders(data_dir: str):
                 "label": seg[0]
             })
 
-        # Define the absolute base path validated on Kaggle
-        base_val = "/kaggle/input/datasets/awsaf49/brats20-dataset-training-validation/BraTS2020_ValidationData/MICCAI_BraTS2020_ValidationData/"
+    # Define the absolute base path validated on Kaggle
+    base_val = "/kaggle/input/datasets/awsaf49/brats20-dataset-training-validation/BraTS2020_ValidationData/MICCAI_BraTS2020_Validation_Data/"
 
-        # Retrieve all patient directories matching the expected pattern
-        patient_dirs_val = sorted(glob.glob(os.path.join(base_val, "BraTS20_Validation_*")))
+    # Retrieve all patient directories matching the expected pattern
+    patient_dirs_val = sorted(glob.glob(os.path.join(base, "BraTS20_Validation_*")))
 
-        data_list_val = []
+    data_list_val = []
 
-        for patient_dir in patient_dirs_val:
-            # Search for the 4 distinct MRI modalities
-            t1 = glob.glob(os.path.join(patient_dir, "*_t1.nii"))
-            t1ce = glob.glob(os.path.join(patient_dir, "*_t1ce.nii"))
-            t2 = glob.glob(os.path.join(patient_dir, "*_t2.nii"))
-            flair = glob.glob(os.path.join(patient_dir, "*_flair.nii"))
+    for patient_dir in patient_dirs_val:
+        # Search for the 4 distinct MRI modalities
+        t1 = glob.glob(os.path.join(patient_dir, "*_t1.nii"))
+        t1ce = glob.glob(os.path.join(patient_dir, "*_t1ce.nii"))
+        t2 = glob.glob(os.path.join(patient_dir, "*_t2.nii"))
+        flair = glob.glob(os.path.join(patient_dir, "*_flair.nii"))
 
-            # Search for the corresponding segmentation mask
-            seg = glob.glob(os.path.join(patient_dir, "*_seg.nii"))
+        # Search for the corresponding segmentation mask
+        seg = glob.glob(os.path.join(patient_dir, "*_seg.nii"))
 
-            # Ensure all 5 files (4 modalities + 1 mask) exist before creating a pair
-            if t1 and t1ce and t2 and flair and seg:
-                data_list_val.append({
-                    # MONAI will automatically stack this list into a 4-channel 3D tensor during data loading
-                    "image": [t1[0], t1ce[0], t2[0], flair[0]],
-                    "label": seg[0]
+        # Ensure all 5 files (4 modalities + 1 mask) exist before creating a pair
+        if t1 and t1ce and t2 and flair and seg:
+            data_list_val.append({
+                # MONAI will automatically stack this list into a 4-channel 3D tensor during data loading
+                "image": [t1[0], t1ce[0], t2[0], flair[0]],
+                "label": seg[0]
                 })
-    return data_list, data_list_val
+
+    return data_list,data_list_val
 
 def get_transforms(roi_size=(64,64,64)):
     """
@@ -107,12 +108,12 @@ def get_transforms(roi_size=(64,64,64)):
 
 def get_dataloader(data_dir:str,batch_size:int=1,roi_size=(64,64,64)):
 
-    data_list=get_bts_data_list(data_dir)
+    data_list,data_list_val=get_bts_data_lists(data_dir)
 
     transforms=get_transforms(roi_size)
 
     dataset=Dataset(data_list,transform=transforms)
-
+    dataset_val = Dataset(data_list, transform=transforms)
     dataloader=DataLoader(
         dataset,
         batch_size=batch_size,
@@ -121,4 +122,12 @@ def get_dataloader(data_dir:str,batch_size:int=1,roi_size=(64,64,64)):
         pin_memory=True
 
     )
-    return dataloader
+    dataloader_val = DataLoader(
+        dataset_val,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=2,
+        pin_memory=True
+
+    )
+    return dataloader,dataloader_val
