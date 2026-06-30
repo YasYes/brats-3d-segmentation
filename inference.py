ceccb81ce2510df6c58d2487a730dbe72372b73c
Yasmina <bouzianeya@cy-tech.fr>
2026-06-30 21:16:19 +0200

    update inference

--- inference.py
+++ inference.py
@@ -3,11 +3,12 @@ import torch
 import nibabel as nib
 import numpy as np
 from monai.inferers import sliding_window_inference
+from monai.metrics import DiceMetric
 
 from model import Brats_model
 
 
-def run_inference(model_path,image_paths,output_path):
+def run_inference(model_path,image_paths,label_paths, output_path):
 
     device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model=Brats_model().to(device)
@@ -15,28 +16,35 @@ def run_inference(model_path,image_paths,output_path):
     model.load_state_dict(torch.load(model_path,map_location=device))
     model.eval()
 
-    data_dict={"image":image_paths}
+    dice_metric = DiceMetric(include_background=False,reduction="mean")
+
+    data_dict={"image":image_paths,"label":label_paths}
 
     loader=monai.transforms.Compose([
-        monai.transforms.LoadImaged(keys=["image"]),
-        monai.transforms.EnsureChannelFirstd(keys=["image"]),
-        monai.transforms.Orientationd(keys=["image"], axcodes = "RAS"),
+        monai.transforms.LoadImaged(keys=["image", "label"]),
+        monai.transforms.EnsureChannelFirstd(keys=["image", "label"]),
+        monai.transforms.Orientationd(keys=["image", "label"], axcodes="RAS"),
         monai.transforms.Spacingd(
-            keys=["image"],
+            keys=["image", "label"],
             pixdim=(1.0, 1.0, 1.0),
-            mode=("bilinear")
+            mode=("bilinear", "nearest")
         ),
         monai.transforms.NormalizeIntensityd(
             keys=["image"],
             nonzero=True,
             channel_wise=True
         ),
-        monai.transforms.EnsureTyped(keys=["image"])
+        monai.transforms.EnsureTyped(keys=["image", "label"])
     ])
 
     loaded_data=loader(data_dict)
 
     image_tensor=loaded_data["image"].unsqueeze(0).to(device)
+    label_tensor = loaded_data["label"].unsqueeze(0).to(device)
+
+    if len(label_tensor.shape) == 4:
+        label_tensor = label_tensor.unsqueeze(1)
+    label_tensor = (label_tensor > 0).float()
 
     with torch.no_grad():
         logits = sliding_window_inference(
@@ -51,6 +59,10 @@ def run_inference(model_path,image_paths,output_path):
     probabilities=torch.sigmoid(logits)
     prediction = (probabilities > 0.75).float()
 
+    dice_metric(y_pred=prediction, y=label_tensor)
+    final_dice = dice_metric.aggregate().item()
+    print(f"✅ Inference complete! 3D Dice Score: {final_dice:.4f}")
+
     pred_numpy=prediction.squeeze(0).cpu().numpy()
 
     if pred_numpy.shape[0]==2:
@@ -60,5 +72,5 @@ def run_inference(model_path,image_paths,output_path):
     nifti_image = nib.Nifti1Image(pred_numpy.astype(np.uint8), original_affine)
     nib.save(nifti_image,output_path)
 
-    return loaded_data["image"][1].cpu().numpy(), pred_numpy
+    return loaded_data["image"][1].cpu().numpy(), pred_numpy, final_dice
 
